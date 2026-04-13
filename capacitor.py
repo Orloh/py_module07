0e38f741b26873291863ec76755edf3705004b20
Orloh <ohercelli@gmail.com>
2026-04-13 17:08:06 +0200

    feat(ex1): implement Healing and Transform creature factories
    - Add HealingCreatureFactory to produce Sproutling and Bloomelle.
    - Add TransformCreatureFactory to produce Shiftling and Morphagon.
    - Inherit from ex0.CreatureFactory to maintain abstract architecture.
    - Ensure factories only expose base and evolved creature types.

---
+++ capacitor.py
@@ -0,0 +1,61 @@
+# *************************************************************************** #
+#                                                                             #
+#                                                        :::      ::::::::    #
+#    capacitor.py                                      :+:      :+:    :+:    #
+#                                                    +:+ +:+         +:+      #
+#    By: orhernan <ohercelli@gmail.com>            +#+  +:+       +#+         #
+#                                                +#+#+#+#+#+   +#+            #
+#    Created: 2026/04/10 22:21:22 by orhernan         #+#    #+#              #
+#    Updated: 2026/04/10 22:25:44 by orhernan        ###   ########.fr        #
+#                                                                             #
+# *************************************************************************** #
+
+from ex1 import HealingCreatureFactory, TransformCreatureFactory
+
+
+def test_healing_factory(factory) -> None:
+    print("Testing Creature with healing capability")
+
+    print("base:")
+    base = factory.create_base()
+    print(base.describe())
+    print(base.attack())
+    print(base.heal(None))
+
+    print("evolved:")
+    evolved = factory.create_evolved()
+    print(evolved.describe())
+    print(evolved.attack())
+    print(evolved.heal(None))
+
+
+def test_transform_factory(factory) -> None:
+    print("\nTesting Creature with transform capability")
+
+    print("base:")
+    base = factory.create_base()
+    print(base.describe())
+    print(base.attack())
+    print(base.transform())
+    print(base.attack())
+    print(base.revert())
+
+    print("evolved:")
+    evolved = factory.create_evolved()
+    print(evolved.describe())
+    print(evolved.attack())
+    print(evolved.transform())
+    print(evolved.attack())
+    print(evolved.revert())
+
+
+def main() -> None:
+    heal_factory = HealingCreatureFactory()
+    transform_factory = TransformCreatureFactory()
+
+    test_healing_factory(heal_factory)
+    test_transform_factory(transform_factory)
+
+
+if __name__ == "__main__":
+    main()
---
+++ ex1/factories.py
@@ -0,0 +1,30 @@
+# *************************************************************************** #
+#                                                                             #
+#                                                        :::      ::::::::    #
+#    factories.py                                      :+:      :+:    :+:    #
+#                                                    +:+ +:+         +:+      #
+#    By: orhernan <ohercelli@gmail.com>            +#+  +:+       +#+         #
+#                                                +#+#+#+#+#+   +#+            #
+#    Created: 2026/04/13 16:57:26 by orhernan         #+#    #+#              #
+#    Updated: 2026/04/13 17:05:55 by orhernan        ###   ########.fr        #
+#                                                                             #
+# *************************************************************************** #
+
+from . import creatures
+from ex0 import CreatureFactory
+
+
+class HealingCreatureFactory(CreatureFactory):
+    def create_base(self) -> creatures.Creature:
+        return creatures.Sproutling()
+
+    def create_evolved(self) -> creatures.Creature:
+        return creatures.Bloomelle()
+
+
+class TransformCreatureFactory(CreatureFactory):
+    def create_base(self) -> creatures.Creature:
+        return creatures.Shiftling()
+
+    def create_evolved(self) -> creatures.Creature:
+        return creatures.Morphagon()
